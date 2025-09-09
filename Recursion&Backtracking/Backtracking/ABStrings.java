8c50d6365e1022be9675815f0af859fcab23da25
Vaishnavi Pandey <139903857+Vaishnavi020404@users.noreply.github.com>
2025-09-10 00:24:15 +0530

    tried to leanr backtracking

---
+++ Recursion&Backtracking/Backtracking/ABStrings.java
@@ -0,0 +1,16 @@
+public class ABStrings {
+    public static void main(String[] args){
+        generate(2,"");
+        
+
+    }
+
+    static void generate(int n,String current){
+        if(current.length()==n){
+            System.out.println(current);
+            return;
+        }
+        generate(n,current+"A");
+        generate(n,current+"B");
+    }
+}
