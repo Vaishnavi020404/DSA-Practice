96ac355852c795e8e70d903c08987f3cc9550f69
Vaishnavi Pandey <139903857+Vaishnavi020404@users.noreply.github.com>
2025-10-19 17:33:11 +0530

    Create Basic.java

---
+++ Recursion&Backtracking/Recursion/Basic.java
@@ -0,0 +1,29 @@
+public class Basic {
+
+    public static void main(String[] args){
+        System.out.println("run a function multiple times without looping or copypasting: ");
+        message1();
+    }
+
+    static void message1(){
+        System.out.println("hey bro i will run message 2 now ");
+        message2();
+    }
+
+    static void message2(){
+        System.out.println("hey bro i will run message 3 now ");
+        message3();
+    }
+    static void message3(){
+        System.out.println("hey bro i will run message 4 now ");
+        message4();
+    }
+
+    static void message4(){
+        System.out.println("done bro");
+    }
+
+
+
+
+}
